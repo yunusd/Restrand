0a0826f64157812c656bc0aac7a2b8270806ae16
Onryld <onryildirm@gmail.com>
2019-09-09 13:07:00 +0300

    ekle ürün menüsü

---
+++ AnaMenu.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Restrand
+{
+    partial class AnaMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.içeceklerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yİYECEKLERToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sOĞUKİÇECEKLERToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sICAKİÇECEKLERToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gÜNEBAŞLARKENToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tATLIKAHVALTILARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aTIŞTIRMALIKLARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sALATALARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tATLILARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aNAYEMEKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kÖYKAHVALTISIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sERPMEKAHVALTIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.içeceklerToolStripMenuItem,
+            this.yİYECEKLERToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1111, 38);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.MenuStrip1_ItemClicked);
+            // 
+            // içeceklerToolStripMenuItem
+            // 
+            this.içeceklerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sOĞUKİÇECEKLERToolStripMenuItem,
+            this.sICAKİÇECEKLERToolStripMenuItem});
+            this.içeceklerToolStripMenuItem.Name = "içeceklerToolStripMenuItem";
+            this.içeceklerToolStripMenuItem.Size = new System.Drawing.Size(125, 34);
+            this.içeceklerToolStripMenuItem.Text = "İÇECEKLER";
+            this.içeceklerToolStripMenuItem.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            // 
+            // yİYECEKLERToolStripMenuItem
+            // 
+            this.yİYECEKLERToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gÜNEBAŞLARKENToolStripMenuItem,
+            this.aTIŞTIRMALIKLARToolStripMenuItem,
+            this.sALATALARToolStripMenuItem,
+            this.tATLILARToolStripMenuItem,
+            this.aNAYEMEKToolStripMenuItem});
+            this.yİYECEKLERToolStripMenuItem.Name = "yİYECEKLERToolStripMenuItem";
+            this.yİYECEKLERToolStripMenuItem.Size = new System.Drawing.Size(136, 34);
+            this.yİYECEKLERToolStripMenuItem.Text = "YİYECEKLER";
+            // 
+            // sOĞUKİÇECEKLERToolStripMenuItem
+            // 
+            this.sOĞUKİÇECEKLERToolStripMenuItem.Name = "sOĞUKİÇECEKLERToolStripMenuItem";
+            this.sOĞUKİÇECEKLERToolStripMenuItem.Size = new System.Drawing.Size(259, 34);
+            this.sOĞUKİÇECEKLERToolStripMenuItem.Text = "SOĞUK İÇECEKLER";
+            // 
+            // sICAKİÇECEKLERToolStripMenuItem
+            // 
+            this.sICAKİÇECEKLERToolStripMenuItem.Name = "sICAKİÇECEKLERToolStripMenuItem";
+            this.sICAKİÇECEKLERToolStripMenuItem.Size = new System.Drawing.Size(259, 34);
+            this.sICAKİÇECEKLERToolStripMenuItem.Text = "SICAK İÇECEKLER";
+            // 
+            // gÜNEBAŞLARKENToolStripMenuItem
+            // 
+            this.gÜNEBAŞLARKENToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tATLIKAHVALTILARToolStripMenuItem,
+            this.kÖYKAHVALTISIToolStripMenuItem,
+            this.sERPMEKAHVALTIToolStripMenuItem});
+            this.gÜNEBAŞLARKENToolStripMenuItem.Name = "gÜNEBAŞLARKENToolStripMenuItem";
+            this.gÜNEBAŞLARKENToolStripMenuItem.Size = new System.Drawing.Size(261, 34);
+            this.gÜNEBAŞLARKENToolStripMenuItem.Text = "GÜNE BAŞLARKEN";
+            // 
+            // tATLIKAHVALTILARToolStripMenuItem
+            // 
+            this.tATLIKAHVALTILARToolStripMenuItem.Name = "tATLIKAHVALTILARToolStripMenuItem";
+            this.tATLIKAHVALTILARToolStripMenuItem.Size = new System.Drawing.Size(272, 34);
+            this.tATLIKAHVALTILARToolStripMenuItem.Text = "TATLI KAHVALTILAR";
+            // 
+            // aTIŞTIRMALIKLARToolStripMenuItem
+            // 
+            this.aTIŞTIRMALIKLARToolStripMenuItem.Name = "aTIŞTIRMALIKLARToolStripMenuItem";
+            this.aTIŞTIRMALIKLARToolStripMenuItem.Size = new System.Drawing.Size(261, 34);
+            this.aTIŞTIRMALIKLARToolStripMenuItem.Text = "ATIŞTIRMALIK";
+            // 
+            // sALATALARToolStripMenuItem
+            // 
+            this.sALATALARToolStripMenuItem.Name = "sALATALARToolStripMenuItem";
+            this.sALATALARToolStripMenuItem.Size = new System.Drawing.Size(261, 34);
+            this.sALATALARToolStripMenuItem.Text = "SALATA";
+            // 
+            // tATLILARToolStripMenuItem
+            // 
+            this.tATLILARToolStripMenuItem.Name = "tATLILARToolStripMenuItem";
+            this.tATLILARToolStripMenuItem.Size = new System.Drawing.Size(261, 34);
+            this.tATLILARToolStripMenuItem.Text = "TATLI";
+            // 
+            // aNAYEMEKToolStripMenuItem
+            // 
+            this.aNAYEMEKToolStripMenuItem.Name = "aNAYEMEKToolStripMenuItem";
+            this.aNAYEMEKToolStripMenuItem.Size = new System.Drawing.Size(261, 34);
+            this.aNAYEMEKToolStripMenuItem.Text = "ANA YEMEK";
+            // 
+            // kÖYKAHVALTISIToolStripMenuItem
+            // 
+            this.kÖYKAHVALTISIToolStripMenuItem.Name = "kÖYKAHVALTISIToolStripMenuItem";
+            this.kÖYKAHVALTISIToolStripMenuItem.Size = new System.Drawing.Size(272, 34);
+            this.kÖYKAHVALTISIToolStripMenuItem.Text = "KÖY KAHVALTISI";
+            // 
+            // sERPMEKAHVALTIToolStripMenuItem
+            // 
+            this.sERPMEKAHVALTIToolStripMenuItem.Name = "sERPMEKAHVALTIToolStripMenuItem";
+            this.sERPMEKAHVALTIToolStripMenuItem.Size = new System.Drawing.Size(272, 34);
+            this.sERPMEKAHVALTIToolStripMenuItem.Text = "SERPME KAHVALTI";
+            // 
+            // AnaMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 597);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "AnaMenu";
+            this.Text = "AnaMenu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem içeceklerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yİYECEKLERToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sOĞUKİÇECEKLERToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sICAKİÇECEKLERToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gÜNEBAŞLARKENToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tATLIKAHVALTILARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kÖYKAHVALTISIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sERPMEKAHVALTIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aTIŞTIRMALIKLARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sALATALARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tATLILARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aNAYEMEKToolStripMenuItem;
+    }
+}
